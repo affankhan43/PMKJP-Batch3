1ab909720b19142148eae21f96740b177a7769e6
affankhan43 <affankhan627@gmail.com>
2022-03-31 20:15:28 +0500

    classes

--- class2/index.js
+++ class2/index.js
@@ -8,22 +8,22 @@ function add(){
 
 
 var firstObj = new animal("Dogs",null,6);
-console.log(firstObj)
-console.log(firstObj.getColor())
-console.log(firstObj.getLegs())
+// console.log(firstObj)
+// console.log(firstObj.getColor())
+// console.log(firstObj.getLegs())
 
 
 var sec = new animal("Cat","Brown",3);
-console.log(sec.getName())
-console.log(sec.getColor())
-console.log(sec.getLegs())
+// console.log(sec.getName())
+// console.log(sec.getColor())
+// console.log(sec.getLegs())
 
 
 
 var trees = new animal("Tiger","Black");
-console.log(trees.getName())
-console.log(trees.getColor())
-console.log(trees.getLegs())
+// console.log(trees.getName())
+// console.log(trees.getColor())
+//console.log(trees.getLegs())
 
 // console.log(addFunc.adddiv(10,10,5))
 //console.log(addFunc.sub(10,10))
@@ -31,3 +31,113 @@ console.log(trees.getLegs())
 // console.log(dao(12,22))
 // console.log(add())
 //console.log(subss(10,10))
+
+
+
+/*
+
+	- slice
+	- substring
+	- substr
+	- indexOf
+	- lastindexOf
+	- startsWith
+	- EndsWith
+	- Math
+	- Inheritance
+
+
+*/
+
+var contentArray = ["hello weo","Pakistan","India"]
+console.log(contentArray.splice(1,1))
+var content = "HelloWorld 123";
+console.log(content.slice(-5,-1))
+
+
+var content2 = "Helloworld Lorem Ipsum.,..."
+console.log(contentArray[0].substring(0,5))
+
+var content3 = "LoremIpsum Random Para"
+
+console.log(content3.substr(6,6))
+
+console.log(content3.indexOf("Para"))
+
+console.log(content3.lastIndexOf("Para",20))
+
+console.log(content3.startsWith("L"))
+
+console.log(content3.endsWith("Para"))
+
+var fileName = "affan.khan,627@gmail.com"
+
+console.log(fileName.split("@")[0].split(",")[0])
+
+class grandParent{
+
+	constructor(gpName,gpDOB){
+		this.gName = gpName
+		this.gDOB = gpDOB
+	}
+
+	combine(){
+		return this.base+' '+this.body
+	}
+
+	gpbio(){
+		return "Name is: "+this.gName+" and Date of birth "+this.gDOB
+	}
+}
+
+class child extends grandParent{
+
+	constructor(grandParentName,grandParentDOB,name,age){
+		super(grandParentName,grandParentDOB);
+		this.name = name;
+		this.age = age
+	}
+
+	SecondBio(){
+		return "Parent Details:  "
+		+this.gpbio()+
+		"Personal Details:  His name is "
+		+this.name+
+		" "+this.gName+
+		" and his age is "+this.age
+	}
+}
+
+
+class grandChild extends child{
+	constructor(grandParentName,grandParentDOB,name,age,gcName,gcDOB){
+		super(grandParentName,grandParentDOB,name,age);
+		this.gcName = gcName
+		this.gcDOB = gcDOB
+	}
+
+	thirdBio(){
+		return this.SecondBio() +
+		"My name is "+this.gcName+" and DOB is" +
+		this.gcDOB
+	}
+
+}
+
+//var newBase = new base();
+var newChild = new grandChild("John","1900-01-01","Mike","1930-03-09","XYZ","2011-09-30");
+
+//console.log("His Parent Details are "+ newChild.gpbio())
+console.log("His details are "+newChild.thirdBio());
+
+
+
+
+
+
+
+
+
+
+
+
