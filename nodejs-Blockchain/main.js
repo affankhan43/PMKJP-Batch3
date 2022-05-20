4a1898f98fa1e729b24947c585f1628b0cba77a4
affankhan43 <affankhan627@gmail.com>
2022-05-20 20:50:40 +0500

    nodejs-blockchainClass2

--- nodejs-Blockchain/main.js
+++ nodejs-Blockchain/main.js
@@ -1,30 +1,31 @@
 var sha256 = require('sha256')
 
 class Block{
-	constructor(timestamp,data,nonce,prevHash){
+	constructor(timestamp,transactions,nonce,prevHash,height){
 		this.timestamp = timestamp
-		this.data = data
+		this.transactions = transactions
 		this.nonce = nonce
 		this.prevHash=prevHash
-		this.hash = this.createHash(timestamp,data,nonce,prevHash)
+		this.height = height
+		this.hash = this.createHash(timestamp,transactions,nonce,prevHash,height)
 	}
 
 
-	createHash(timestamp,data,nonce,prevHash){
-		//var nonce = this.proofOfWork(timestamp,data,prevHash);
-		return sha256(timestamp+JSON.stringify(data)+nonce+prevHash)
+	createHash(timestamp,transactions,nonce,prevHash,height){
+		//var nonce = this.proofOfWork(timestamp,transactions,prevHash);
+		return sha256(timestamp+JSON.stringify(transactions)+nonce+prevHash+height)
 	}
 
-	static proofOfWork(timestamp,data,prevHash){
+	static proofOfWork(timestamp,transactions,prevHash,height){
 		var nonce = 0;
-		var difficulty = "001"
+		var difficulty = "1111"
 		var success = true
 		while(success){
 			nonce++;
-			var hash = sha256(timestamp+JSON.stringify(data)+nonce+prevHash)
+			var hash = sha256(timestamp+JSON.stringify(transactions)+nonce+prevHash+height)
 			
 			//console.log(hash.slice(0,3))
-			if(hash.slice(0,3) == difficulty){
+			if(hash.slice(0,4) == difficulty){
 				success = false;
 				console.log("Hurray! We Got Correct Nonce --> "+ nonce)
 			}
@@ -40,23 +41,52 @@ class Block{
 class Blockchain{
 	constructor(){
 		this.chain = [this.createGenesisBlock()];
+		this.mempool = [];
 	}
 
 	createGenesisBlock(){
 		var time = Date.now().toString();
-		var nonce = Block.proofOfWork(time,{},"0")
-		var newblock = new Block(time,{},nonce,"0");
+		var nonce = Block.proofOfWork(time,this.mempool,"0",1)
+		var newblock = new Block(time,this.mempool,nonce,"0",1);
 		console.log('Hi! We create Genesis')
+		this.mempool = []
 		return newblock;
 	}
 
-	addNewBlock(data){
+	mineNewBlock(){
 		var time = Date.now().toString();
 		var prevhx = this.chain[this.chain.length-1].hash;
-		var nonce = Block.proofOfWork(time,data,prevhx)
-		var newbloc = new Block(time,data,nonce,prevhx)
+		var nonce = Block.proofOfWork(time,this.mempool,prevhx,this.chain.length+1)
+		var newbloc = new Block(time,this.mempool,nonce,prevhx,this.chain.length+1)
 		this.chain.push(newbloc)
 		console.log("Hi! We got new blocknumber # "+(this.chain.length-1))
+		this.mempool = []
+	}
+
+
+	createTx(fromAddress, toAddress, value){
+		var tx = {"from":fromAddress,"to":toAddress,"amount":value}
+		this.mempool.push(tx)
+	}
+
+	isChainValid(){
+		var valid = true
+		if(this.chain[0].prevHash != "0" || this.chain[0].height != 1){
+			return false;
+		}
+		for (var i = 1; i < this.chain.length; i++) {
+			if(this.chain[i].height != i+1){
+				return false
+			}
+			if(this.chain[i-1].hash != this.chain[i].prevHash){
+				return false
+			}
+			if(this.chain[i].hash != sha256(this.chain[i].timestamp+JSON.stringify(this.chain[i].transactions)+this.chain[i].nonce+this.chain[i].prevHash+this.chain[i].height)){
+				return false;
+			}
+		}
+
+		return valid;
 	}
 
 }
@@ -64,15 +94,19 @@ class Blockchain{
 
 
 var xyzNetwork = new Blockchain();
-xyzNetwork.addNewBlock({"value":"asdsd"})
+xyzNetwork.mineNewBlock()
+
+xyzNetwork.createTx("address1","address2",123)
+xyzNetwork.createTx("address2","address3",543)
 //xyzNetwork.addNewBlock({"value":"asdsd"})
 //xyzNetwork.addNewBlock({"value":"asdsd"})
 
 
 
-
-
+xyzNetwork.mineNewBlock()
+xyzNetwork.chain[2].transactions[0].from = "affan"
 console.log(xyzNetwork)
+console.log("Is chain Valid--->>  "+xyzNetwork.isChainValid())
 
 
 
