96386ce64b9c760095666bd699851d65cae80392
affankhan43 <affankhan627@gmail.com>
2022-05-29 17:11:39 +0500

    update

--- nodejs-Blockchain/main.js
+++ nodejs-Blockchain/main.js
@@ -1,4 +1,5 @@
 var sha256 = require('sha256')
+var { v4:uuidv4 } = require('uuid')
 
 class Block{
 	constructor(timestamp,transactions,nonce,prevHash,height){
@@ -18,14 +19,14 @@ class Block{
 
 	static proofOfWork(timestamp,transactions,prevHash,height){
 		var nonce = 0;
-		var difficulty = "1111"
+		var difficulty = "111"
 		var success = true
 		while(success){
 			nonce++;
 			var hash = sha256(timestamp+JSON.stringify(transactions)+nonce+prevHash+height)
 			
 			//console.log(hash.slice(0,3))
-			if(hash.slice(0,4) == difficulty){
+			if(hash.slice(0,3) == difficulty){
 				success = false;
 				console.log("Hurray! We Got Correct Nonce --> "+ nonce)
 			}
@@ -39,17 +40,22 @@ class Block{
 //console.log(newblock)
 
 class Blockchain{
+	mempool = []
+	chain = []
 	constructor(){
 		this.chain = [this.createGenesisBlock()];
-		this.mempool = [];
+		//this.mempool = [];
 	}
 
 	createGenesisBlock(){
 		var time = Date.now().toString();
-		var nonce = Block.proofOfWork(time,this.mempool,"0",1)
-		var newblock = new Block(time,this.mempool,nonce,"0",1);
+		var nonce = Block.proofOfWork(time,[],"0",1)
+		var newblock = new Block(time,[],nonce,"0",1);
 		console.log('Hi! We create Genesis')
-		this.mempool = []
+		//this.mempool = []
+		//reward transaction
+		this.createTx("0x00000","Affan",10);
+		//console.log(this.mempool)
 		return newblock;
 	}
 
@@ -61,12 +67,25 @@ class Blockchain{
 		this.chain.push(newbloc)
 		console.log("Hi! We got new blocknumber # "+(this.chain.length-1))
 		this.mempool = []
+		//reward transaction
+		this.createTx("0x00000","Affan",10);
 	}
 
 
 	createTx(fromAddress, toAddress, value){
-		var tx = {"from":fromAddress,"to":toAddress,"amount":value}
-		this.mempool.push(tx)
+		var fromBalance = this.getBalance(fromAddress)
+		if(fromBalance > value || fromAddress == "0x00000"){
+			var tx = {
+				"txid":uuidv4().split('-').join(""),
+				"from":fromAddress,
+				"to":toAddress,
+				"amount":value
+			}
+			this.mempool.push(tx)
+		}else{
+			console.log("insuffcient Balance")
+		}
+		
 	}
 
 	isChainValid(){
@@ -89,10 +108,40 @@ class Blockchain{
 		return valid;
 	}
 
+	getTransactions(){
+		var txs = [];
+		//9
+		for (var i = 0; i < this.chain.length; i++) {
+			if(typeof(this.chain[i].transactions) != "undefined" && this.chain[i].transactions.length > 0 ){
+				console.log("We have some txs in block height --> "+ this.chain[i].height)
+				for (var j = 0; j < this.chain[i].transactions.length; j++) {
+					var tempTx = this.chain[i].transactions[j]
+					tempTx.blockHeight = this.chain[i].height;
+					tempTx.confirmations = this.chain.length - i;
+					txs.push(tempTx)
+				}
+			}
+		}
+		return txs;
+	}
+
+	getBalance(address){
+		var txs = this.getTransactions();
+		var balance = 0;
+		for (var i = 0; i < txs.length; i++) {
+			if(txs[i].from == address){
+				balance -= txs[i].amount;
+			}
+			else if(txs[i].to == address){
+				balance = balance + txs[i].amount
+			}
+		}
+		return balance;
+	}
+
 }
 
-
-
+//console.log(uuidv4().split('-').join(""))
 var xyzNetwork = new Blockchain();
 xyzNetwork.mineNewBlock()
 
@@ -104,9 +153,16 @@ xyzNetwork.createTx("address2","address3",543)
 
 
 xyzNetwork.mineNewBlock()
-xyzNetwork.chain[2].transactions[0].from = "affan"
-console.log(xyzNetwork)
-console.log("Is chain Valid--->>  "+xyzNetwork.isChainValid())
+xyzNetwork.createTx("Affan","address3",1)
+xyzNetwork.mineNewBlock()
+xyzNetwork.mineNewBlock()
+xyzNetwork.mineNewBlock()
+// xyzNetwork.chain[2].transactions[0].from = "affan"
+//console.log(xyzNetwork)
+console.log(xyzNetwork.getTransactions())
+console.log(xyzNetwork.getBalance("Affan"))
+console.log(xyzNetwork.chain[xyzNetwork.chain.length-1].height)
+//console.log("Is chain Valid--->>  "+xyzNetwork.isChainValid())
 
 
 
